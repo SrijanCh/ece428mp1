049893ba9905222fc5fec3d967e7539359fd7ddf
Srijan Chakraborty <srijanc2@illinois.edu>
2019-09-14 19:44:16 -0600

    tweaked the server starting script

--- scripts/srijanc2/gostart.sh
+++ scripts/srijanc2/gostart.sh
@@ -7,23 +7,20 @@ while [ $i -lt 11 ]; do
     echo "Sshing to fa19-cs425-g77-$num.cs.illinois.edu"
     sshpass -f pass.txt ssh srijanc2@"fa19-cs425-g77-$num.cs.illinois.edu" "cd; 
         if [ $1 == "setup" ]; then
-            # mkdir go_work; 
-            echo 'export GOPATH=/home/srijanc2/go_work' >> .bashrc; 
+            mkdir go_work; 
+            echo 'GOPATH=$HOME/go_work/src' >> .bashrc; 
             cd go_work;
-            # git clone https://github.com/SrijanCh/ece428mp1 src;
-        
+            git clone https://github.com/SrijanCh/ece428mp1 src;
         elif [ $1 == "update" ]; then
             cd go_work/src
             git pull
+        elif [ $1 == "killhost" ]; then
+            killall host
         elif [ $1 == "start" ]; then
-            cd go_work
-            go build client
-            go build host
-            ./host
-            if [ $i == $2 ]; then
-                echo "Client is VM $i"
-                ./client
-            fi           
+            cd go_work/src/mp1
+            go build client.go
+            go build host.go
+            nohup ./host &>/dev/null &
         else
             echo "Unrecognized command line option"
         fi
