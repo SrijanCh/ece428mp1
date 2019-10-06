7b401f7d0e82585b68e051340b7be67988885fba
Srijan Chakraborty <srijanc2@illinois.edu>
2019-10-06 07:59:31 -0600

    minor shit

--- mp2/node.go
+++ mp2/node.go
@@ -411,11 +411,12 @@ func init_() {
         mem_table = intro_info.table
         neigh = beatable.Reval_table(my_node_hash, mem_table)   
     }       
-        if neigh[0] == -1 || neigh[1] == -1 || neigh[2] == -1 || neigh[3] == -1{
-            fmt.Printf("Can't get neigh\n")
-            return
-        }
+    fmt.Printf("Our node is initialized! This node is hashed to " + string(my_node_hash) + " with node_id " + my_node_id.IPV4_addr.String() + ":" + string(my_node_id.Timestamp) + ".\n")
 
+    if neigh[0] == -1 || neigh[1] == -1 || neigh[2] == -1 || neigh[3] == -1{
+        fmt.Printf("Can't get neigh\n")
+        return
+    }
 
 }
 
