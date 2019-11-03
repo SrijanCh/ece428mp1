a26f8b64b47896d31737fdc073f425d038a06d79
Joey <jbahary2@illinois.edu>
2019-11-02 23:50:53 -0500

    Added end script

---
+++ mp3/end.sh
@@ -0,0 +1,12 @@
+i=1
+while [ $i -lt 11 ]; do
+    num=$i
+    if [ $i -lt 10 ]; then
+       num="0$i" 
+    fi
+    echo "Sshing to fa19-cs425-g77-$num.cs.illinois.edu"
+    sshpass -f ~/pass.txt ssh srijanc2@"fa19-cs425-g77-$num.cs.illinois.edu" "cd; 
+    killall -u srijanc2
+    "
+    i=$((i+1))
+done 
