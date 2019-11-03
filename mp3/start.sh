019bacce59e768681427d7853c508c4d698e2c5c
Joey <jbahary2@illinois.edu>
2019-11-02 23:36:49 -0500

    Added start script

---
+++ mp3/start.sh
@@ -0,0 +1,14 @@
+i=1
+while [ $i -lt 11 ]; do
+    num=$i
+    if [ $i -lt 10 ]; then
+       num="0$i" 
+    fi
+    echo "Sshing to fa19-cs425-g77-$num.cs.illinois.edu"
+    sshpass -f ~/pass.txt ssh srijanc2@"fa19-cs425-g77-$num.cs.illinois.edu" "cd; 
+    export GOPATH=/home/srijanc2/go_work/
+    cd /home/srijanc2/go_work/src/mp3/
+    go run membership.go -logfile=vmlog.txt
+    "
+    i=$((i+1))
+done 
