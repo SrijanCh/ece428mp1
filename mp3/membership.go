258fc511373bd10b6b9909adf19fcdc2763bbfbf
Srijan Chakraborty <srijanc2@illinois.edu>
2019-11-02 19:54:30 -0600

    scrooge mcduck

--- mp3/membership.go
+++ mp3/membership.go
@@ -1204,6 +1204,8 @@ func (t *Zookeeper) Zoo_put(args Put_args, reply *Put_return) error {
 	//Just a check-in for a handled request
 	if fileloc_arr, ok := FileTable[args.Sdfsname]; !ok { //NEW PUT
 		a, b := pick4()
+		fmt.Println("a: ", a)
+		fmt.Println("b: ", b)
 		c := int64(time.Now().Nanosecond())
 		var f [4]FileLoc
 		f[0] = FileLoc{a[0], b[0], c}
