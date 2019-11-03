080da54746a2c1f14649f5f4a45dba3ef503679c
Joey <jbahary2@illinois.edu>
2019-11-03 17:41:53 -0600

    Fixing put confirm

--- mp3/membership.go
+++ mp3/membership.go
@@ -1391,14 +1391,18 @@ func put_confirm (sdfsname string) bool {
     var ts int64
     ts = 0
     // find latest updated node
-    for i  := 0; i < 4; i++ {
-        ts = maxtime(ts, FileTable[sdfsname][i].Timestamp)
+    if _, ok := FileTable[sdfsname]; ok {
+        for i  := 0; i < 4; i++ {
+            ts = maxtime(ts, FileTable[sdfsname][i].Timestamp)
+        }
+        // If the time for the latest updated node is less than a minute ago, send the warning message by replying with true
+        if ts - time.Now().UnixNano() < 60000000000 {
+            return true
+        }
+        return false
+    } else {
+        return false
     }
-    // If the time for the latest updated node is less than a minute ago, send the warning message by replying with true
-    if ts - time.Now().UnixNano() < 60000000000 {
-        return true
-    }
-    return false
 }
 
 func del(filename, ip, port string) int64{
